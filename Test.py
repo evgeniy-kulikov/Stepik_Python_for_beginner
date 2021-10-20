91efe3c1b6c75526dd081d2c77ef9d649109733b
evgeniy-kulikov <redstepway@gmail.com>
2021-10-20 15:44:50 +0300

    add file 07.2

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@
 # input:
 # output:
 # Другое решение
+# Чужое решение
 # Задача 01:
 # a, b, c = int(input('')), int(input('')), int(input(''))
 # a, b = float(input('')), float(input(''))
@@ -10,9 +11,13 @@
 # n = int(input(''))
 # a, b, c = (input('')), (input('')), (input(''))
 # x, y, x_1, y_1 = int(input()), int(input()), int(input()), int(input())
+# 5 x 1 = 5
+n = int(input())
+for i in range(1, 11):
+    print(f'{n} x {i} =', n * i)
 
-
-
-
+n = int(input())
+for i in range(1, 11):
+    print(f'{n} x {i} = {n*i}')
 
 
